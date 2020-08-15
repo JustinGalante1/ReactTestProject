0cc35fda0cd5bfb2ce5eb20600ea4f2ff71e6c39
JustinGalante1 <jn2wf@virginia.edu>
2020-08-15 16:35:28 -0400

    Added getUserDetails, markNotificationsRead

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,10 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
+const {db} = require('./util/admin');
+
 const { getScream, getAllScreams, postOneScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead } = require('./handlers/users');
 
 //scream routes
 app.get('/screams', getAllScreams);
@@ -22,5 +24,74 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
+    .onCreate((snapshot)=> {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+        .get()
+        .then( doc => {
+            if(doc.exists){
+                return db.doc(`/notifications/${snapshot.id}`)
+                    .set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id
+                    });
+            }
+        })
+        .then(() => {
+            return;
+        })
+        .catch(err => {
+            console.error(err);
+            return;
+        });
+    });
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
+    .onCreate((snapshot)=> {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+        .get()
+        .then( doc => {
+            if(doc.exists){
+                return db.doc(`/notifications/${snapshot.id}`)
+                    .set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: doc.id
+                    });
+            }
+        })
+        .then(() => {
+            return;
+        })
+        .catch(err => {
+            console.error(err);
+            return;
+        });
+    });
+
+exports.deleteNotificationOnUnLie = functions.firestore.document('likes/{id}')
+    .onDelete((snapshot) => {
+        db
+            .doc(`/notifications/${snapshot.id}`)
+            .delete()
+            .then(() => {
+                return;
+            })
+            .catch(err =>{
+                console.error(err);
+                return;
+            })
+
+    })
